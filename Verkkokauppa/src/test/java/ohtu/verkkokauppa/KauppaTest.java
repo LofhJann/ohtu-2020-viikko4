49a368f3bce9d01a5772c02e4b29835d76b54023
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-11-16 16:26:56 +0200

    Add more tests

--- Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -7,22 +7,37 @@ import static org.mockito.Mockito.*;
 
 public class KauppaTest {
 
-    @Test
-    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-        // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
+    private Pankki pankki;
+    private Viitegeneraattori viite;
+    private Varasto varasto;
+    private Kauppa k;
 
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+
+
+    @Before
+    public void setUp() {
+        // luodaan ensin mock-oliot
+        pankki = mock(Pankki.class);
+
+        viite = mock(Viitegeneraattori.class);
         // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
+        varasto = mock(Varasto.class);
         // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.saldo(2)).thenReturn(15);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(1, "leipä", 10));
+        when(varasto.saldo(3)).thenReturn(0);
+        when(varasto.haeTuote(3)).thenReturn(new Tuote(1, "powerking", 1000));
 
         // sitten testattava kauppa
-        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k = new Kauppa(varasto, pankki, viite);
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
 
         // tehdään ostokset
         k.aloitaAsiointi();
@@ -32,5 +47,53 @@ public class KauppaTest {
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+
+    @Test
+    public void ostoksetLaskutetaanOikealtaAsiakkaalta() {
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+
+    @Test
+    public void useampiTuoteToimii() {
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(15));
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+
+    @Test
+    public void useampiSamaTuoteToimii() {
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(10));
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+
+    @Test
+    public void tuoteOnLoppunut() {
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(3);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));
     }
 }
