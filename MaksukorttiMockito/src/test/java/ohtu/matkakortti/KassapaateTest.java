e22827d48a96ad3e94d9be7beecab4421022ba2b
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-11-16 15:48:10 +0200

    Fix lataa

--- MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -5,6 +5,7 @@ import ohtu.matkakortti.Maksukortti;
 import ohtu.matkakortti.Kassapaate;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
@@ -13,21 +14,21 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class KassapaateTest {
-    
+
     Kassapaate kassa;
     Maksukortti kortti;
-    
+
     @Before
     public void setUp() {
         kassa = new Kassapaate();
         kortti = mock(Maksukortti.class);
     }
-    
+
     @Test
     public void kortiltaVelotetaanHintaJosRahaaOn() {
         when(kortti.getSaldo()).thenReturn(10);
         kassa.ostaLounas(kortti);
-        
+
         verify(kortti, times(1)).getSaldo();
         verify(kortti).osta(eq(Kassapaate.HINTA));
     }
@@ -36,9 +37,25 @@ public class KassapaateTest {
     public void kortiltaEiVelotetaJosRahaEiRiita() {
         when(kortti.getSaldo()).thenReturn(4);
         kassa.ostaLounas(kortti);
-        
+
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
-      
+
+    @Test
+    public void kortilleLadataanSaldoaJosSummaOnPositiivinen() {
+        when(kortti.getSaldo()).thenReturn(0);
+        kassa.lataa(kortti, 10);
+
+        verify(kortti, times(1)).lataa(anyInt());
+    }
+
+    @Test
+    public void kortilleEiLadataSaldoaJosSummaOnNegatiivinen() {
+        when(kortti.getSaldo()).thenReturn(0);
+        kassa.lataa(kortti, -3);
+
+        verify(kortti, times(0)).lataa(anyInt());
+    }
+
 }
